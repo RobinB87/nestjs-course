6a9cb3090b360a35fbe81ee35c0505c20db98aaf
RobinB87 <robinbette@hotmail.com>
2023-11-04 17:29:34 +0000

    connect the various services and modules

--- di/src/cpu/cpu.service.ts
+++ di/src/cpu/cpu.service.ts
@@ -3,5 +3,11 @@ import { PowerService } from 'src/power/power.service';
 
 @Injectable()
 export class CpuService {
-  constructor(private readonly PowerService: PowerService) {}
+  constructor(private readonly powerService: PowerService) {}
+
+  compute(a: number, b: number) {
+    console.log('Drawing 10 watts of power from Power Service');
+    this.powerService.supplyPower(10);
+    return a + b;
+  }
 }
