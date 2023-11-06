aa491538d20eb2ae5cbaadc7856ff6a300f08b11
RobinB87 <robinbette@hotmail.com>
2023-11-06 10:50:40 +0000

    setup crud methods with typeorm's methods which call the hooks

--- car-value/src/users/users.service.ts
+++ car-value/src/users/users.service.ts
@@ -1,7 +1,7 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import { User } from './user.entity';
-import { InjectRepository } from '@nestjs/typeorm';
 
 @Injectable()
 export class UsersService {
@@ -13,5 +13,28 @@ export class UsersService {
   create(email: string, password: string) {
     const user = this.repo.create({ email, password });
     return this.repo.save(user);
+  }
+
+  async findOne(id: number) {
+    const user = await this.repo.findOneBy({ id });
+    if (!user) throw new NotFoundException('User not found');
+    return user;
+  }
+
+  async find(email: string) {
+    const users = await this.repo.find({ where: { email } });
+    if (!users.length) throw new NotFoundException('User not found');
+    return users;
+  }
+
+  async update(id: number, attrs: Partial<User>) {
+    const user = await this.findOne(id);
+    Object.assign(user, attrs);
+    return this.repo.save(user);
+  }
+
+  async remove(id: number) {
+    const user = await this.findOne(id);
+    return this.repo.remove(user);
   }
 }
