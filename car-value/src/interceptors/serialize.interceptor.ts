6b107d7155d9c3720fea7de657f56af80de17ce0
RobinB87 <robinbette@hotmail.com>
2023-11-06 13:18:58 +0000

    intercept data and return a userDto without password

--- car-value/src/interceptors/serialize.interceptor.ts
+++ car-value/src/interceptors/serialize.interceptor.ts
@@ -1,16 +1,16 @@
 import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
+import { plainToClass } from 'class-transformer';
 import { Observable, map } from 'rxjs';
+import { UserDto } from 'src/users/dtos/user.dto';
 
 export class SerializeInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-    // run something before a request is handled by the request handler
-    console.log('running before the handler', context);
-
     return next.handle().pipe(
-      map((data) => {
-        // run something before the response is sent out
-        console.log('running before response is sent out', data);
-      }),
+      map((data) =>
+        plainToClass(UserDto, data, {
+          excludeExtraneousValues: true,
+        }),
+      ),
     );
   }
 }
