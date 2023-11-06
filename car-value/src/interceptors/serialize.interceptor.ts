80dad2dc464ef0a482d7f82a2c973b4cf6c06f60
RobinB87 <robinbette@hotmail.com>
2023-11-06 13:24:17 +0000

    make more generic

--- car-value/src/interceptors/serialize.interceptor.ts
+++ car-value/src/interceptors/serialize.interceptor.ts
@@ -1,13 +1,14 @@
 import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
-import { plainToClass } from 'class-transformer';
+import { plainToInstance } from 'class-transformer';
 import { Observable, map } from 'rxjs';
-import { UserDto } from 'src/users/dtos/user.dto';
 
 export class SerializeInterceptor implements NestInterceptor {
+  constructor(private dto: any) {}
+
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     return next.handle().pipe(
       map((data) =>
-        plainToClass(UserDto, data, {
+        plainToInstance(this.dto, data, {
           excludeExtraneousValues: true,
         }),
       ),
