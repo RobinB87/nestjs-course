105a4db8ef0842c54d506bb2f9909f2cabd9d7dd
RobinB87 <robinbette@hotmail.com>
2023-11-07 16:10:02 +0000

    finalize the controller tests

--- car-value/src/users/users.controller.spec.ts
+++ car-value/src/users/users.controller.spec.ts
@@ -11,15 +11,18 @@ describe('UsersController', () => {
 
   beforeEach(async () => {
     fakeUsersService = {
-      findOne: () => Promise.resolve({} as User),
-      find: () => Promise.resolve([]),
+      findOne: (id: number) =>
+        Promise.resolve({ id, email: 'bla@bla.com', password: '123' } as User),
+      find: (email: string) =>
+        Promise.resolve([{ id: 1, email, password: '123' } as User]),
       update: () => Promise.resolve({} as User),
       remove: () => Promise.resolve({} as User),
     };
 
     fakeAuthService = {
       signup: () => Promise.resolve({} as User),
-      signin: () => Promise.resolve({} as User),
+      signin: (email: string, password: string) =>
+        Promise.resolve({ id: 1, email, password } as User),
     };
 
     const module: TestingModule = await Test.createTestingModule({
@@ -35,5 +38,28 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('getAllByEmail - should return all users with a given email', async () => {
+    const users = await controller.getAllByEmail('bla@bla.com');
+    expect(users.length).toEqual(1);
+    expect(users[0].email).toEqual('bla@bla.com');
+  });
+
+  it('getOne - should return a single user with the given id', async () => {
+    const user = await controller.getOne('5');
+    expect(user).toBeDefined();
+    expect(user.id).toEqual(5);
+  });
+
+  it('signin - should update session object and return user', async () => {
+    const session = { userId: 0 };
+    const user = await controller.signin(
+      { email: 'bla@bla.com', password: '123' },
+      session,
+    );
+
+    expect(user.id).toEqual(1);
+    expect(session.userId).toEqual(1);
   });
 });
