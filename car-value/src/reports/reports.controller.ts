79244e6e08f4c21bd8e983f85d9fc8b4db4638c9
RobinB87 <robinbette@hotmail.com>
2023-11-08 09:47:00 +0000

    fix creation of report

--- car-value/src/reports/reports.controller.ts
+++ car-value/src/reports/reports.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
+import { AuthGuard } from 'src/guards/auth.guard';
 import { CreateReportDto } from './dtos/create-report-dto';
 import { ReportsService } from './reports.service';
 
@@ -7,7 +8,8 @@ export class ReportsController {
   constructor(private readonly reportsService: ReportsService) {}
 
   @Post()
+  @UseGuards(AuthGuard)
   create(@Body() body: CreateReportDto) {
-    console.log('b', body);
+    return this.reportsService.create(body);
   }
 }
--- car-value/src/reports/reports.service.ts
+++ car-value/src/reports/reports.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
+import { CreateReportDto } from './dtos/create-report-dto';
 import { Report } from './report.entity';
 
 @Injectable()
@@ -8,4 +9,9 @@ export class ReportsService {
   constructor(
     @InjectRepository(Report) private readonly repo: Repository<Report>,
   ) {}
+
+  create(create: CreateReportDto) {
+    const report = this.repo.create(create);
+    return this.repo.save(report);
+  }
 }
