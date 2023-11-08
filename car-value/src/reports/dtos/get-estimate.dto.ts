32a50b15f745e894624324439670b53fd3abee79
RobinB87 <robinbette@hotmail.com>
2023-11-08 12:22:47 +0000

    get estimate by querystring

--- car-value/src/reports/dtos/get-estimate.dto.ts
+++ car-value/src/reports/dtos/get-estimate.dto.ts
@@ -6,34 +6,33 @@ import {
   Max,
   Min,
 } from 'class-validator';
+import { million } from './create-report.dto';
+import { Transform } from 'class-transformer';
 
-export const million: number = 1000000;
-
-export class CreateReportDto {
-  @IsNumber()
-  @Min(0)
-  @Max(million)
-  price: number;
-
+export class GetEstimateDto {
   @IsString()
   make: string;
 
   @IsString()
   model: string;
 
+  @Transform(({ value }) => +value)
   @IsNumber()
   @Min(1930)
   @Max(2050)
   year: number;
 
+  @Transform(({ value }) => parseFloat(value))
   @IsNumber()
   @IsLongitude()
   lng: number;
 
+  @Transform(({ value }) => parseFloat(value))
   @IsNumber()
   @IsLatitude()
   lat: number;
 
+  @Transform(({ value }) => +value)
   @IsNumber()
   @Min(0)
   @Max(million)
--- car-value/src/reports/reports.controller.ts
+++ car-value/src/reports/reports.controller.ts
@@ -1,9 +1,11 @@
 import {
   Body,
   Controller,
+  Get,
   Param,
   Patch,
   Post,
+  Query,
   UseGuards,
 } from '@nestjs/common';
 import { AdminGuard } from 'src/guards/admin.guard';
@@ -13,6 +15,7 @@ import { CurrentUser } from 'src/users/decorators/current-user.decorator';
 import { User } from 'src/users/user.entity';
 import { ApproveReportDto } from './dtos/approve-report.dto';
 import { CreateReportDto } from './dtos/create-report.dto';
+import { GetEstimateDto } from './dtos/get-estimate.dto';
 import { ReportDto } from './dtos/report.dto';
 import { ReportsService } from './reports.service';
 
@@ -32,4 +35,7 @@ export class ReportsController {
   approve(@Param('id') id: string, @Body() body: ApproveReportDto) {
     return this.reportsService.changeApproval(id, body.approved);
   }
+
+  @Get()
+  getEstimate(@Query() query: GetEstimateDto) {}
 }
